28251d97149cd16d9191a4bbd17ea27bcd247d1e
Darach Gorham <darachg22@gmail.com>
2023-10-12 10:51:49 +0100

    updated notebook and Task 1. I have added a true and false dataframe for the next part of the task and also created a range to 10000 in order to test the numbers

--- Task1.py
+++ Task1.py
@@ -5,9 +5,6 @@
 #3x + 1 otherwise
 #For example, starting with the value 10, which is an even number, we divide it by 2 to get 5. Then 5 is an odd number so, we mul tiply by 3 and add 1 to get 16. Then we repeatedly divide by 2 to get 8, 4, 2, 1. Once we are at 1, we go back to 4 and get stuck in the repeating sequence 4, 2, 1 as we suspected.
 #Your task is to verify, using Python, that the conjecture is true for the first 10,000 positive integers
-
-
-#My code which I completed last semester that returns the numbers found: 
 def Collatz(n):
     out=[n]
     while n>1:
@@ -17,21 +14,13 @@ def Collatz(n):
             n = (n*3)+1
         out.append(int(n))
     return out
+#https://realpython.com/python-boolean/
+true = []
+false = []
 
-n = int(input("Enter a number:"))
-result = Collatz(n)
-print(*result)
+#altering code for task
+for i in range(1,10000): 
+    Verif = Collatz(i) #https://www.freecodecamp.org/news/python-for-loop-for-i-in-range-example/ 
+    
 
-#I now need to alter the code to fit the brief i.e.verify, using Python, that the conjecture is true for the first 10,000 positive integers
-#source used: https://stackoverflow.com/questions/13366830/collatz-conjecture-sequence
-#             https://www.sanfoundry.com/python-program-test-collatz-conjecture-given-number/
-
-def Collatz(n):
-    out=[n]
-    while n>1:
-        if n%2==0:
-           n = n//2
-        else:
-            n = (n*3)+1
-        out.append(int(n))
-    return out
+    
